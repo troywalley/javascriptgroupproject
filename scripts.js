7c009ead64b95de4a18d3a121630169396d69108
Troy Walley <walley.troy@yahoo.com>
2017-04-14 13:43:18 -0400

    finished part 1

--- scripts.js
+++ scripts.js
@@ -1,14 +1,106 @@
 var numbers = document.getElementsByClassName("num");
 var calcInput = document.getElementsByClassName("calc-input")[0];
-
+var x=0
+var y=0
+var answer=false
+var currentoperator=""
 for (var i = 0; i < numbers.length; i++) {
 	numbers[i].addEventListener("click", function(event) {
+		if(answer===true){
+			calcInput.innerHTML=""
+			answer=false
+		}
 		calcInput.innerHTML += event.target.innerHTML;
+		console.log(calcInput);
 	});
 }
+// var operatorRow = document.getElementsByClassName("operator-row");
+// console.log(operatorRow.length)
+var operator = document.getElementsByClassName("operator");
+operator[0].addEventListener("click", function(){
+	currentoperator=operator[0].innerHTML;
+	
+	x=calcInput.innerHTML;
+	calcInput.innerHTML="";
 
-var operator;
+})
+operator[1].addEventListener("click", function(){
+	alert["operator1"]
+	currentoperator=operator[1].innerHTML;
+	x=calcInput.innerHTML;
+	calcInput.innerHTML="";	
+
+})
+operator[2].addEventListener("click", function(){
+	currentoperator=operator[2].innerHTML;
+	x=calcInput.innerHTML;
+	calcInput.innerHTML="";	
+
+})
+operator[3].addEventListener("click", function(){
+	currentoperator=operator[3].innerHTML;
+	x=calcInput.innerHTML;
+	calcInput.innerHTML="";	
+
+})
+operator[4].addEventListener("click", function(){
+	
+	y=calcInput.innerHTML;
+	calculate(x,y,currentoperator)
+
+})
+
+// for(var i=0;i<operator.length;i++){
+	
+
+// 	operator[i].addEventListener("click", function(){
+	
+// 		if(x===""){
+			
+// 		x=calcInput.innerHTML;
+// 		calcInput.innerHTML="";	
+// 		var currentoperator=operator[i].innerHTML;
+// 		console.log(currentoperator)
+// 	}else{
+// 		y=calcInput.innerHTML;
+// 		calcInput.innerHTML="";
+		
+// 		calculate(x,y,operator[i])
+// 	}
+		
+// 	});
+// 	// var currentoperator=operator[].innerHTML
+// 	// 		console.log(currentoperator)
+// }
 
 function calculate(x, y, operator) {
+	console.log(x)
+	console.log(y)
+	console.log(operator)
+	answer=true;
+	if(operator==="-"){
+		calcInput.innerHTML=parseInt(x)-parseInt(y)
+		x=0;
+		y=0;
+		currentoperator=""
+	}
+	if(operator==="+"){
+		calcInput.innerHTML=parseInt(x)+parseInt(y)
+		x=0;
+		y=0;
+		currentoperator=""
+	}
+	if(operator==="x"){
+		calcInput.innerHTML=parseInt(x)*parseInt(y)
+		x=0;
+		y=0;
+		currentoperator=""
+	}
+	if(operator==="/"){
+		calcInput.innerHTML=parseInt(x)/parseInt(y)
+		x=0;
+		y=0;
+		currentoperator=""
+	}
 
 }
